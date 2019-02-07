4d051d0bcf9062ec02f5551de7508e7ff219b6e3
Daniel Fryar <daniel.fryar@gmail.com>
2019-02-07 15:53:19 -0600

    Done with step 1

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,8 +7,16 @@ import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+
+    public static final String USER = "user";
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        // if logged in, redirect to /profile
+        if (request.getSession().getAttribute(LoginServlet.USER) != null) {
+            response.sendRedirect("/profile");
+        } else {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -17,6 +25,7 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            request.getSession().setAttribute(USER, "user");
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
